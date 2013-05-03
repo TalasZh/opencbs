2602214f4b0904a28cedc589fb550ec6fc0c10b4
Pavel Bastov <pbastov@gmail.com>
2013-05-03 09:16:39 +0600

    Fix the restarter main form.

--- Src/OpenCBS.GUI.Restarter/MainForm.Designer.cs
+++ Src/OpenCBS.GUI.Restarter/MainForm.Designer.cs
@@ -30,21 +30,23 @@ namespace OpenCBS.GUI.Restarter
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.pictureBoxAboutOctopus = new System.Windows.Forms.PictureBox();
+            this.pbSplash = new System.Windows.Forms.PictureBox();
             this.timerMain = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAboutOctopus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSplash)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBoxAboutOctopus
+            // pbSplash
             // 
-            this.pictureBoxAboutOctopus.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pictureBoxAboutOctopus.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxAboutOctopus.Image")));
-            this.pictureBoxAboutOctopus.Location = new System.Drawing.Point(0, 0);
-            this.pictureBoxAboutOctopus.Name = "pictureBoxAboutOctopus";
-            this.pictureBoxAboutOctopus.Size = new System.Drawing.Size(613, 231);
-            this.pictureBoxAboutOctopus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBoxAboutOctopus.TabIndex = 1;
-            this.pictureBoxAboutOctopus.TabStop = false;
+            this.pbSplash.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pbSplash.ErrorImage = null;
+            this.pbSplash.Image = global::OpenCBS.GUI.Restarter.Properties.Resources.logo;
+            this.pbSplash.InitialImage = null;
+            this.pbSplash.Location = new System.Drawing.Point(0, 0);
+            this.pbSplash.Name = "pbSplash";
+            this.pbSplash.Size = new System.Drawing.Size(341, 138);
+            this.pbSplash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbSplash.TabIndex = 1;
+            this.pbSplash.TabStop = false;
             // 
             // timerMain
             // 
@@ -57,10 +59,10 @@ namespace OpenCBS.GUI.Restarter
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(612, 231);
+            this.ClientSize = new System.Drawing.Size(340, 138);
             this.ControlBox = false;
-            this.Controls.Add(this.pictureBoxAboutOctopus);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Controls.Add(this.pbSplash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -68,7 +70,7 @@ namespace OpenCBS.GUI.Restarter
             this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAboutOctopus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSplash)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,7 +78,7 @@ namespace OpenCBS.GUI.Restarter
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBoxAboutOctopus;
+        private System.Windows.Forms.PictureBox pbSplash;
         private System.Windows.Forms.Timer timerMain;
     }
 }
