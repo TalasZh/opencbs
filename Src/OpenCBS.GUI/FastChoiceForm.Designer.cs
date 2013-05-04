05ae0ad94beeea9ca2e51b73db3f81a5536a27d7
Pavel Bastov <pbastov@gmail.com>
2013-05-04 18:35:37 +0600

    Implement a first draft version of the default dashboard.

--- Src/OpenCBS.GUI/FastChoiceForm.Designer.cs
+++ Src/OpenCBS.GUI/FastChoiceForm.Designer.cs
@@ -28,12 +28,82 @@ namespace OpenCBS.GUI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FastChoiceForm));
             this.generalInfoPanel = new System.Windows.Forms.Panel();
+            this.infoPanel = new System.Windows.Forms.Panel();
+            this.quickLinksPanel = new System.Windows.Forms.Panel();
+            this.newCorporateClientLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.newNonSolidairtyGroupLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.newSolidarityGroupLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.newIndividualClientLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.clientsLabel = new System.Windows.Forms.Label();
+            this.generalInfoPanel.SuspendLayout();
+            this.quickLinksPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // generalInfoPanel
             // 
+            this.generalInfoPanel.Controls.Add(this.infoPanel);
+            this.generalInfoPanel.Controls.Add(this.quickLinksPanel);
             resources.ApplyResources(this.generalInfoPanel, "generalInfoPanel");
             this.generalInfoPanel.Name = "generalInfoPanel";
+            // 
+            // infoPanel
+            // 
+            this.infoPanel.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.infoPanel, "infoPanel");
+            this.infoPanel.Name = "infoPanel";
+            // 
+            // quickLinksPanel
+            // 
+            this.quickLinksPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.quickLinksPanel.Controls.Add(this.newCorporateClientLinkLabel);
+            this.quickLinksPanel.Controls.Add(this.newNonSolidairtyGroupLinkLabel);
+            this.quickLinksPanel.Controls.Add(this.newSolidarityGroupLinkLabel);
+            this.quickLinksPanel.Controls.Add(this.newIndividualClientLinkLabel);
+            this.quickLinksPanel.Controls.Add(this.clientsLabel);
+            resources.ApplyResources(this.quickLinksPanel, "quickLinksPanel");
+            this.quickLinksPanel.Name = "quickLinksPanel";
+            // 
+            // newCorporateClientLinkLabel
+            // 
+            this.newCorporateClientLinkLabel.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.newCorporateClientLinkLabel, "newCorporateClientLinkLabel");
+            this.newCorporateClientLinkLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(151)))), ((int)(((byte)(234)))));
+            this.newCorporateClientLinkLabel.Name = "newCorporateClientLinkLabel";
+            this.newCorporateClientLinkLabel.TabStop = true;
+            this.newCorporateClientLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnCorporateClientLinkLabelLinkClick);
+            // 
+            // newNonSolidairtyGroupLinkLabel
+            // 
+            this.newNonSolidairtyGroupLinkLabel.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.newNonSolidairtyGroupLinkLabel, "newNonSolidairtyGroupLinkLabel");
+            this.newNonSolidairtyGroupLinkLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(151)))), ((int)(((byte)(234)))));
+            this.newNonSolidairtyGroupLinkLabel.Name = "newNonSolidairtyGroupLinkLabel";
+            this.newNonSolidairtyGroupLinkLabel.TabStop = true;
+            this.newNonSolidairtyGroupLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnNewNonSolidairtyGroupLinkLabelLinkClick);
+            // 
+            // newSolidarityGroupLinkLabel
+            // 
+            this.newSolidarityGroupLinkLabel.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.newSolidarityGroupLinkLabel, "newSolidarityGroupLinkLabel");
+            this.newSolidarityGroupLinkLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(151)))), ((int)(((byte)(234)))));
+            this.newSolidarityGroupLinkLabel.Name = "newSolidarityGroupLinkLabel";
+            this.newSolidarityGroupLinkLabel.TabStop = true;
+            this.newSolidarityGroupLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnNewSolidarityGroupLinkLabelLinkClick);
+            // 
+            // newIndividualClientLinkLabel
+            // 
+            this.newIndividualClientLinkLabel.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.newIndividualClientLinkLabel, "newIndividualClientLinkLabel");
+            this.newIndividualClientLinkLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(151)))), ((int)(((byte)(234)))));
+            this.newIndividualClientLinkLabel.Name = "newIndividualClientLinkLabel";
+            this.newIndividualClientLinkLabel.TabStop = true;
+            this.newIndividualClientLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnNewIndividualClientLinkLabelClick);
+            // 
+            // clientsLabel
+            // 
+            resources.ApplyResources(this.clientsLabel, "clientsLabel");
+            this.clientsLabel.ForeColor = System.Drawing.Color.White;
+            this.clientsLabel.Name = "clientsLabel";
             // 
             // FastChoiceForm
             // 
@@ -45,14 +115,21 @@ namespace OpenCBS.GUI
             this.ShowInTaskbar = false;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.OnLoad);
+            this.generalInfoPanel.ResumeLayout(false);
+            this.quickLinksPanel.ResumeLayout(false);
+            this.quickLinksPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
         #endregion
 
         private System.Windows.Forms.Panel generalInfoPanel;
-
-
-
+        private System.Windows.Forms.Panel quickLinksPanel;
+        private System.Windows.Forms.Panel infoPanel;
+        private System.Windows.Forms.Label clientsLabel;
+        private System.Windows.Forms.LinkLabel newIndividualClientLinkLabel;
+        private System.Windows.Forms.LinkLabel newNonSolidairtyGroupLinkLabel;
+        private System.Windows.Forms.LinkLabel newSolidarityGroupLinkLabel;
+        private System.Windows.Forms.LinkLabel newCorporateClientLinkLabel;
     }
 }
--- Src/OpenCBS.GUI/FastChoiceForm.cs
+++ Src/OpenCBS.GUI/FastChoiceForm.cs
@@ -1,9 +1,11 @@
 ﻿// LICENSE PLACEHOLDER
 
+using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using OpenCBS.Enums;
+using OpenCBS.GUI.Clients;
 using OpenCBS.GUI.UserControl;
-using OpenCBS.CoreDomain;
-using OpenCBS.Shared;
 
 namespace OpenCBS.GUI
 {
@@ -16,6 +18,102 @@ namespace OpenCBS.GUI
 
         private void OnLoad(object sender, System.EventArgs e)
         {
+            CreatePortfolioPieChart();
+            CreateParPieChart();
+        }
+
+        private void CreatePortfolioPieChart()
+        {
+            var chart = new Chart();
+            var chartArea = new ChartArea();
+            chart.ChartAreas.Add(chartArea);
+
+            var series = new Series();
+            series.ChartType = SeriesChartType.Pie;
+            var point = series.Points.Add(95);
+            point.LegendText = "Performing: 95%";
+            point.Color = Color.FromArgb(28, 151, 234);
+            point = series.Points.Add(5);
+            point.LegendText = "PAR: 5%";
+            point.Color = Color.FromArgb(234, 28, 28);
+
+            var title = new Title
+            {
+                Text = "Portfolio",
+                ForeColor = Color.FromArgb(45, 45, 48),
+                Font = new Font("Arial", 9.75f)
+            };
+            chart.Titles.Add(title);
+
+            var legend = new Legend();
+            legend.Docking = Docking.Right;
+            legend.Alignment = StringAlignment.Center;
+            chart.Legends.Add(legend);
+
+            chart.Series.Add(series);
+            chart.Location = new Point(10, 10);
+            chart.Size = new Size(250, 150);
+
+            infoPanel.Controls.Add(chart);
+        }
+
+        private void CreateParPieChart()
+        {
+            var chart = new Chart();
+            var chartArea = new ChartArea();
+            chart.ChartAreas.Add(chartArea);
+
+            var series = new Series();
+            series.ChartType = SeriesChartType.Pie;
+            series.Points.Add(95).LegendText = "Performing: 95%";
+            series.Points.Add(5).LegendText = "PAR: 5%";
+
+            var title = new Title
+            {
+                Text = "PAR",
+                ForeColor = Color.FromArgb(45, 45, 48),
+                Font = new Font("Arial", 9.75f)
+            };
+            chart.Titles.Add(title);
+
+            var legend = new Legend();
+            legend.Docking = Docking.Right;
+            legend.Alignment = StringAlignment.Center;
+            chart.Legends.Add(legend);
+
+            chart.Series.Add(series);
+            chart.Location = new Point(270, 10);
+            chart.Size = new Size(250, 150);
+
+            infoPanel.Controls.Add(chart);
+        }
+
+        private void OpenClientForm(OClientTypes clientType)
+        {
+            var parent = Application.OpenForms[0];
+            var form = new ClientForm(clientType, parent, false) { MdiParent = parent };
+            form.Show();            
+        }
+
+        private void OnNewIndividualClientLinkLabelClick(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenClientForm(OClientTypes.Person);
+        }
+
+        private void OnNewSolidarityGroupLinkLabelLinkClick(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenClientForm(OClientTypes.Group);
+        }
+
+        private void OnNewNonSolidairtyGroupLinkLabelLinkClick(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var form = new NonSolidaryGroupForm { MdiParent = Application.OpenForms[0] };
+            form.Show();
+        }
+
+        private void OnCorporateClientLinkLabelLinkClick(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenClientForm(OClientTypes.Corporate);
         }
     }
 }
