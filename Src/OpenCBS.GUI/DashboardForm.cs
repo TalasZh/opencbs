d8939476bd3564d27bfcb0e2429c18d255b8c038
Pavel Bastov <pbastov@gmail.com>
2013-05-07 16:38:16 +0600

    Add title to the dashboard charts.

--- Src/OpenCBS.GUI/DashboardForm.cs
+++ Src/OpenCBS.GUI/DashboardForm.cs
@@ -22,9 +22,9 @@ namespace OpenCBS.GUI
         private Chart _olbTrendChart;
 
         public DashboardForm()
-		{
+        {
             InitializeComponent();
-		}
+        }
 
         private void OnLoad(object sender, EventArgs e)
         {
@@ -36,20 +36,20 @@ namespace OpenCBS.GUI
 
             activityPefrormedAtColumn.AspectToStringConverter = value =>
             {
-                var date = (DateTime) value;
+                var date = (DateTime)value;
                 return date.ToString("dd.MM.yyyy");
             };
 
-            activityAmountColumn.AspectToStringConverter = 
+            activityAmountColumn.AspectToStringConverter =
             parAmountColumn.AspectToStringConverter = value =>
             {
-                var amount = (decimal) value;
+                var amount = (decimal)value;
                 return amount.ToString("N2", numberFormatInfo);
             };
 
             parListView.RowFormatter = listViewItem =>
             {
-                var portfolioLine = (PortfolioLine) listViewItem.RowObject;
+                var portfolioLine = (PortfolioLine)listViewItem.RowObject;
                 if (portfolioLine.Name == "Total")
                 {
                     var font = listViewItem.Font;
@@ -70,7 +70,7 @@ namespace OpenCBS.GUI
             var chartArea = new ChartArea();
             _portfolioChart.ChartAreas.Add(chartArea);
 
-            var parPercentage = 0 == dashboard.Olb ? 0 : Math.Round(100*dashboard.Par/dashboard.Olb, 1);
+            var parPercentage = 0 == dashboard.Olb ? 0 : Math.Round(100 * dashboard.Par / dashboard.Olb, 1);
             var performingPercentage = 100 - parPercentage;
 
             var numberFormatInfo = new NumberFormatInfo
@@ -85,18 +85,13 @@ namespace OpenCBS.GUI
             point.LegendText = string.Format("Performing: {0} %", performingPercentage.ToString("N1", numberFormatInfo));
             //point.Color = Color.FromArgb(28, 151, 234);
             point.Color = Color.FromArgb(72, 234, 28);
-            
+
             point = series.Points.Add(Convert.ToDouble(parPercentage));
             point.LegendText = string.Format("PAR: {0} %", parPercentage.ToString("N1", numberFormatInfo));
             point.Color = Color.FromArgb(234, 28, 28);
 
-            var legend = new Legend
-            {
-                Docking = Docking.Right, 
-                Alignment = StringAlignment.Center,
-                Font = new Font("Arial", 7f),
-            };
-            _portfolioChart.Legends.Add(legend);
+            AddLegend(_portfolioChart);
+            AddTitle(_portfolioChart, "Portfolio");
 
             _portfolioChart.Series.Add(series);
             _portfolioChart.Location = new Point(0, 0);
@@ -142,20 +137,15 @@ namespace OpenCBS.GUI
             series.ChartType = SeriesChartType.Pie;
             for (var i = 0; i < 6; i++)
             {
-                var value = Math.Round(0 == dashboard.Par ? 0 : 100*values[i]/dashboard.Par, 1);
+                var value = Math.Round(0 == dashboard.Par ? 0 : 100 * values[i] / dashboard.Par, 1);
                 var point = series.Points.Add(Convert.ToDouble(value));
                 var numberFormat = Math.Round(value) == value ? "N0" : "N1";
                 point.LegendText = string.Format("{0}: {1}%", legends[i], value.ToString(numberFormat));
                 point.Color = colors[i];
             }
 
-            var legend = new Legend
-            {
-                Docking = Docking.Right,
-                Alignment = StringAlignment.Center,
-                Font = new Font("Arial", 7f),
-            };
-            _parChart.Legends.Add(legend);
+            AddLegend(_parChart);
+            AddTitle(_parChart, "PAR");
 
             _parChart.Series.Add(series);
             _parChart.Location = new Point(0, 0);
@@ -186,6 +176,7 @@ namespace OpenCBS.GUI
 
             _disbursementsChart.Series.Add(series);
             _disbursementsChart.Dock = DockStyle.Fill;
+            AddTitle(_disbursementsChart, "Number of disbursements in last 10 days");
 
             disbursementsPanel.Controls.Add(_disbursementsChart);
         }
@@ -218,17 +209,39 @@ namespace OpenCBS.GUI
 
             _olbTrendChart.Series.Add(series);
             _olbTrendChart.Dock = DockStyle.Fill;
+            AddTitle(_olbTrendChart, "OLB trend in last 10 days");
 
             olbTrendPanel.Controls.Add(_olbTrendChart);
         }
 
+        private void AddLegend(Chart chart)
+        {
+            var legend = new Legend
+            {
+                Docking = Docking.Right,
+                Alignment = StringAlignment.Center,
+                Font = new Font("Arial", 7f),
+            };
+            chart.Legends.Add(legend);
+        }
 
+        private void AddTitle(Chart chart, string text)
+        {
+            var title = new Title
+            {
+                Text = text,
+                Font = new Font("Arial", 7f, FontStyle.Bold),
+                ForeColor = Color.FromArgb(45, 45, 48),
+                Docking = Docking.Bottom,
+            };
+            chart.Titles.Add(title);
+        }
 
         private void OpenClientForm(OClientTypes clientType)
         {
             var parent = Application.OpenForms[0];
             var form = new ClientForm(clientType, parent, false) { MdiParent = parent };
-            form.Show();            
+            form.Show();
         }
 
         private void OnNewIndividualClientLinkLabelClick(object sender, LinkLabelLinkClickedEventArgs e)
