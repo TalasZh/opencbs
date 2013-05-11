753efdafced2bb021de99a6ed2bd72d5ae583f18
Pavel Bastov <pbastov@gmail.com>
2013-05-11 17:25:43 +0600

    Add proper permissions to the registry.

--- Src/OpenCBS.Installer/build.bat
+++ Src/OpenCBS.Installer/build.bat
@@ -1,2 +1,2 @@
-candle.exe opencbs.wxs
-light.exe -b "d:\build\Src\OpenCBS.GUI\bin\Release" -ext WiXNetFxExtension -ext WiXUIExtension opencbs.wixobj
+candle.exe -ext WixUtilExtension opencbs.wxs
+light.exe -b "d:\build\Src\OpenCBS.GUI\bin\Release" -ext WixNetFxExtension -ext WixUIExtension -ext WixUtilExtension opencbs.wixobj
